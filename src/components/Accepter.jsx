3afb412e688650ee7c3ac4b5af4957221d1635b1
Mireg-V <markgerasimchuk8@gmail.com>
2024-03-25 20:49:10 +0100

    .

--- src/components/Accepter.jsx
+++ src/components/Accepter.jsx
@@ -6,7 +6,7 @@ export function Accepter() {
       <img src='smile-emoji.webp' alt='' />
       <p>Here is the place to place my experience in your company.</p>
       <h6>
-        <span>Made by</span>Mireg<span>from</span><img src='./impactium.svg' className={s.impactium} alt=''/>Impactium<span>with</span><img className={s.heart} src='./heart.svg' alt=''/></h6>
+        <span>Made by</span>Mireg<span>from</span><img src='impactium.svg' className={s.impactium} alt=''/>Impactium<span>with</span><img className={s.heart} src='heart.svg' alt=''/></h6>
     </div>
   );
 }
